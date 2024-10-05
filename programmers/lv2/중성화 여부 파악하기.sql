0b21f9deec73a10a47d86dbb2b16de9948c74a2f
Kimbumsoo99 <show7441@naver.com>
2024-10-05 17:24:43 +0900

    PRG SQL 연습

---
+++ programmers/lv2/중성화 여부 파악하기.sql
@@ -0,0 +1,8 @@
+SELECT animal_id, name, 
+  case 
+    when sex_upon_intake like '%Spayed%' 
+      or sex_upon_intake like '%Neutered%' then 'O' 
+    else 'X' 
+  end as 중성화 
+from animal_ins 
+order by 1;
